7f346389e5c21254557c1cfa703dae6b9b5c92db
Bojan SKI <codeucenje@gmail.com>
2025-05-26 17:21:50 +0200

    custom middleware IsBidOwnerMiddleware created. bids route updated. IsOwnerMiddleware renamed to IsProjectOwnerMiddleware. bids & projects routes updated

--- routes/bids.php
+++ routes/bids.php
@@ -4,14 +4,19 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BidController;
 use App\Http\Middleware\FreelancerUserMiddleware;
 use App\Http\Middleware\ClientUserMiddleware;
+use App\Http\Middleware\IsBidOwnerMiddleware;
+use App\Http\Middleware\IsProjectOwnerMiddleware;
 
 Route::middleware('auth')->group(function () {
     Route::middleware(FreelancerUserMiddleware::class)->group(function () {
         Route::post('/{project}/bid_for_project', [BidController::class, 'store'])->name('freelancer.bid.store');
-        Route::delete('/{bid}/destroy', [BidController::class, 'destroy'])->name('freelancer.bid.destroy');
+
+        Route::middleware(IsBidOwnerMiddleware::class)->group(function () {
+            Route::delete('/{bid}/destroy', [BidController::class, 'destroy'])->name('freelancer.bid.destroy');
+        });
     });
 
-    Route::middleware(ClientUserMiddleware::class)->group(function () {
+    Route::middleware([ClientUserMiddleware::class, IsProjectOwnerMiddleware::class])->group(function () {
         Route::prefix('projects')->group(function () {
             Route::put('{project}/{bid}/accept', [BidController::class, 'accept'])->name('client.bid.accept');
             Route::put('{project}/{bid}/reject', [BidController::class, 'reject'])->name('client.bid.reject');
--- routes/projects.php
+++ routes/projects.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\SearchController;
-use App\Http\Middleware\IsOwnerMiddleware;
+use App\Http\Middleware\IsProjectOwnerMiddleware;
 
 Route::middleware('auth')->group(function () {
     Route::prefix('projects')->group(function () {
@@ -12,7 +12,7 @@ Route::middleware('auth')->group(function () {
         Route::get('/create', [ProjectController::class, 'create'])->name('projects.create');
         Route::post('/store', [ProjectController::class, 'store'])->name('projects.store');
 
-        Route::middleware(IsOwnerMiddleware::class)->group(function () {
+        Route::middleware(IsProjectOwnerMiddleware::class)->group(function () {
             Route::get('/{project}/edit', [ProjectController::class, 'edit'])->name('projects.edit');
             Route::put('/{project}/update', [ProjectController::class, 'update'])->name('projects.update');
             Route::delete('/{project}/destroy', [ProjectController::class, 'destroy'])->name('projects.destroy');
